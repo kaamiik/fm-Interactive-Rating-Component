d07a9d653b13632ebba8bcc9fd827b477bd0b5a8
Kamran Kiani <kiani.km@gmail.com>
2024-03-14 17:24:23 +0330

    Update JS files

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 "use strict";
 
+// No need of this code just put it here to archive
 // Constant Variables
 const formEl = document.querySelector(".feedback-form");
 const inputEls = [...document.querySelectorAll(".feedback-form__button")];
 
 // Uncheck all radio buttons when the page loads
-window.onload = function () {
-  inputEls.forEach((input) => {
-    input.checked = false;
-  });
-};
+// window.onload = function () {
+//   inputEls.forEach((input) => {
+//     input.checked = false;
+//   });
+// };
 
-formEl.addEventListener("submit", function (e) {
-  e.preventDefault();
-  inputEls.forEach((input) => {
-    if (input.checked) {
-      localStorage.setItem("rating", input.value);
-      window.location.href = "thanks.html"; // redirect to thanks.html page
-    }
-  });
-});
+// formEl.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   inputEls.forEach((input) => {
+//     if (input.checked) {
+//       localStorage.setItem("rating", input.value);
+//       window.location.href = "thanks.html"; // redirect to thanks.html page
+//     }
+//   });
+// });
--- thanks.js
+++ thanks.js
@@ -1,7 +1,14 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function () {
-  const rating = localStorage.getItem("rating"); // get the rating from localStorage
-  const ratingMessage = document.querySelector(".thank-you__rating");
-  ratingMessage.textContent = `You selected ${rating} out of 5`;
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   const rating = localStorage.getItem("rating"); // get the rating from localStorage
+//   const ratingMessage = document.querySelector(".thank-you__rating");
+//   ratingMessage.textContent = `You selected ${rating} out of 5`;
+// });
+
+const ratingMessage = document.querySelector(".thank-you__rating");
+const searchParams = new URLSearchParams(window.location.search);
+console.log(searchParams);
+const rating = searchParams.get("rating");
+
+ratingMessage.textContent = `You selected ${rating} out of 5`;
